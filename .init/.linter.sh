7b85ae04036952da07cc3c2ca7ca8f10f51877aa
Kavia AI <production@kavia.ai>
2025-07-04 06:42:36 +0000

    CheckPoint

---
+++ .init/.linter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/wellnesssync-120519-120528/frontend_app
+npm run lint
+ESLINT_EXIT_CODE=$?
+npm run build
+BUILD_EXIT_CODE=$?
+if [ $ESLINT_EXIT_CODE -ne 0 ] || [ $BUILD_EXIT_CODE -ne 0 ]; then
+   exit 1
+fi
+
